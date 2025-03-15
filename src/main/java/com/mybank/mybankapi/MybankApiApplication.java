89d6968a0f1c8f82ff2a6325212bcd984466f448
nikopa96 <jertyk@gmail.com>
2025-03-15 21:16:18 +0200

    Project initialization

---
+++ src/main/java/com/mybank/mybankapi/MybankApiApplication.java
@@ -0,0 +1,13 @@
+package com.mybank.mybankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankApiApplication.class, args);
+	}
+
+}
