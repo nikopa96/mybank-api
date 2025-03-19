9c4287e95d055f9db11855b8562b80d11f437ac5
nikopa96 <jertyk@gmail.com>
2025-03-19 02:28:34 +0200

    Exception handlers for Account Service methods

--- src/main/java/com/mybank/mybankapi/service/AccountService.java
+++ src/main/java/com/mybank/mybankapi/service/AccountService.java
@@ -3,22 +3,30 @@ package com.mybank.mybankapi.service;
 import com.mybank.accountservice.model.AccountTotalResponse;
 import com.mybank.api.model.AccountTotalRequestApiModel;
 import com.mybank.api.model.AccountTotalResponseApiModel;
+import com.mybank.mybankapi.exception.AccountServiceClientException;
 import com.mybank.mybankapi.integration.accountservice.AccountServiceClient;
-import com.mybank.mybankapi.mapper.AccountMapper;
+import com.mybank.mybankapi.mapper.ResponseMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AccountService {
 
     private final AccountServiceClient accountServiceClient;
-    private final AccountMapper accountMapper;
+    private final ResponseMapper responseMapper;
 
     public AccountTotalResponseApiModel getBankAccountTotal(AccountTotalRequestApiModel request) {
-        AccountTotalResponse clientResponse = accountServiceClient
-                .getBankAccountTotal(request.getIban(), request.getCurrencyCode().getValue());
+        try {
+            AccountTotalResponse clientResponse = accountServiceClient
+                    .getBankAccountTotal(request.getIban(), request.getCurrencyCode().getValue());
 
-        return accountMapper.toApiModel(clientResponse);
+            return responseMapper.toApiModel(clientResponse);
+        } catch (AccountServiceClientException e) {
+            log.error("Account not found by IBAN: {}", request.getIban());
+            throw new IllegalArgumentException();
+        }
     }
 }
